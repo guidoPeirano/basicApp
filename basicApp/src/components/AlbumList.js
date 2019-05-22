ed64d439865ea87c1e5ebe091513fc3e199022c7
Guido Peirano <guidopeirano316@gmail.com>
2019-05-22 12:06:28 -0300

    Creacion de tarjetas

--- basicApp/src/components/AlbumList.js
+++ basicApp/src/components/AlbumList.js
@@ -1,23 +1,41 @@
 //Import stuff
+//Class Based component have behavoirs
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import AlbumDetail from './AlbumDetail'
 
 //Create Component
 class AlbumList extends Component{
+	state = {
+		albums: []
+	};//Initial or Empty State
+
 	componentWillMount() {
 		fetch('https://rallycoding.herokuapp.com/api/music_albums')
 			.then(response => response.json())
 			.then(response => {
 				console.log(response)
+				this.setState({ albums: response });
 			})	
 
 	}
 
+	renderAlbums(){
+		return this.state.albums.map(album =>{
+			return(//Key, must be unique it doesnt rerenders it
+				<AlbumDetail key={album.title} album={album}/>
+			)	
+		})
+	}
+
 	render(){
+		console.log(this.state)
+
 		return(
-			<View>
-	 			<Text>Album List!!! </Text>
-			</View>
+			<ScrollView>
+
+	 			{this.renderAlbums()}
+			</ScrollView>
 		);
 	}
 };
