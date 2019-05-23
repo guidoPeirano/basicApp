8c635fb9a5cd2c49e79a714c07e07db4073d6b4a
Guido Peirano <guidopeirano316@gmail.com>
2019-05-22 23:19:34 -0300

    Se agrega el servidor a la aplicacion

--- basicApp/src/components/AlbumDetail.js
+++ basicApp/src/components/AlbumDetail.js
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { View, Text, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
+import axios from 'axios';
 
-const AlbumDetail = ({ album }) => {
-	const  {title, artist, thumbnail_image, image, url} = album;
-	const {thumbnailStyle, headerContentStyle, imageStyle, headerTextStyle,thumbnailContainerStyle} = styles;
-	return (
-		<Card>
-			<CardSection>
-				<View style={thumbnailContainerStyle}>
-					<Image 
-					style={thumbnailStyle}
-					source={{ uri : thumbnail_image}}/>
-				</View>
-				<View style={headerContentStyle}>
-					<Text style={headerTextStyle}>{title}</Text>
-					<Text>{artist}</Text>
-				</View>
-			</CardSection>
-			<CardSection>
-					<Image 
-					style={imageStyle}
-					source={{ uri: image}}/>
-			</CardSection>
-			<CardSection>
-				<Button onPress={() => console.log(title)}>
-					Consulta
-				</Button>
-				<Button onPress={() => Linking.openURL(url)}>
-					Ok
-				</Button>
-			</CardSection>
-		</Card>
-	)
+class AlbumDetail extends Component {
+	render(){
+		const { album } = this.props
+		const  {title, artist, thumbnail_image, image, url} = album;
+		const {thumbnailStyle, headerContentStyle, imageStyle, headerTextStyle,thumbnailContainerStyle} = styles;
+		return (
+			<Card>
+				<CardSection>
+					<View style={thumbnailContainerStyle}>
+						<Image 
+						style={thumbnailStyle}
+						source={{ uri : thumbnail_image}}/>
+					</View>
+					<View style={headerContentStyle}>
+						<Text style={headerTextStyle}>{title}</Text>
+						<Text>{artist}</Text>
+					</View>
+				</CardSection>
+				<CardSection>
+						<Image 
+						style={imageStyle}
+						source={{ uri: image}}/>
+				</CardSection>
+				<CardSection>
+					<Button onPress={() => axios.get('http://localhost:3000').then(console.log)}>
+						Consulta
+					</Button>
+					<Button onPress={() => Linking.openURL(url)}>
+						Ok
+					</Button>
+				</CardSection>
+			</Card>
+		)	
+	}
+	
 };
 
 const styles = {
